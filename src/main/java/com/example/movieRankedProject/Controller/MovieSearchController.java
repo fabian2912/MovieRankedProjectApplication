0060396400a16df455e0222914e8ccafdad39dd2
diablolaw <94478511+diablolaw@users.noreply.github.com>
2023-02-02 16:21:51 +0000

    refactor backend & add rest controller

---
+++ src/main/java/com/example/movieRankedProject/Controller/MovieSearchController.java
@@ -0,0 +1,80 @@
+package com.example.movieRankedProject.Controller;
+
+import com.example.movieRankedProject.Model.Movie;
+import com.example.movieRankedProject.Service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.websocket.server.PathParam;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieSearchController {
+
+    Logger logger = LoggerFactory.getLogger(MovieSearchController.class);
+
+    @Autowired
+    private MovieService movieService;
+//
+//    @GetMapping
+//    public Movie get(){
+//        return new Movie();
+//    }
+
+    @GetMapping(value = {"/{movieTitle}"})
+    public Movie  getSearchedMovie(@PathVariable String movieTitle) throws IOException {
+        System.out.println("Get reached");
+        String unrefinedTitle = movieTitle;
+        String[] splitTitle = unrefinedTitle.split(Pattern.quote(" "));
+        String title = "";
+        for (int i = 0; i < splitTitle.length; i++) {
+            System.out.println("    " + splitTitle[i]);
+            String newTitle = splitTitle[i].substring(0,1).toUpperCase() + splitTitle[i].substring(1).toLowerCase();
+            title = title + newTitle + " ";
+        } // making sure the title is in Title Case to check for uniqueness
+        title = title.substring(0, title.length()-1);
+
+
+        System.err.println("in post with title " + title);
+
+        MovieSearchParser movieSearchParser = new MovieSearchParser();
+
+
+            logger.info("movie Controller - passing over to MovieSearchParser");
+
+            Movie movie1 = null;
+
+            if (movieService.findByTitle(title) != null) {
+                System.err.println("movie " + title + " is already in the database; incrementing the vote counter");
+                movie1 = movieService.findByTitle(title);
+
+            } else if (movieService.findByTitle(title) == null) {
+                System.err.println("no similar title found");
+                movie1 = movieSearchParser.findMovieByTitle(title);
+                if (movie1.getTitle() != null && movie1.getCountry() != null) { // checking that api returned valid movie
+                    movie1 = movieService.save(movie1);
+                    System.err.println("valid movie added");
+                }
+                else {
+                    System.err.println("api incorrectly returned title");
+                }
+            } else {
+                System.err.println(title + " is already included in the database");
+            }
+        logger.info("exiting post");
+        return movie1;
+        }
+
+    }
+
--- src/main/java/com/example/movieRankedProject/Model/Movie.java
+++ src/main/java/com/example/movieRankedProject/Model/Movie.java
@@ -2,11 +2,17 @@ package com.example.movieRankedProject.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table
+@Data
+@AllArgsConstructor
+//@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Movie {
 
@@ -40,9 +46,12 @@ public class Movie {
     @JsonProperty("Actors")
     private String Actors;
 
+    @JsonProperty("Poster")
+    private String Poster;
+
     private int votes = 1;
 
-    public Movie(String title, String year, String country, String rated, String director, String awards, String boxOffice, String plot, String actors, int votes) {
+    public Movie(String title, String year, String country, String rated, String director, String awards, String boxOffice, String plot, String actors, String poster, int votes) {
         Title = title;
         Year = year;
         Country = country;
@@ -52,40 +61,10 @@ public class Movie {
         BoxOffice = boxOffice;
         Plot = plot;
         Actors = actors;
+        Poster = poster;
         this.votes = votes;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String title) {
-        Title = title;
-    }
-
-    public String getYear() {
-        return Year;
-    }
-
-    public void setYear(String year) {
-        Year = year;
-    }
-
-    public String getCountry() {
-        return Country;
-    }
-
-    public void setCountry(String country) {
-        Country = country;
-    }
 
     public Movie(Long id, String title, String year, String country) {
         this.id = id;
@@ -108,59 +87,4 @@ public class Movie {
         System.out.println("default empty constructor");
     }
 
-    public String getRated() {
-        return Rated;
-    }
-
-    public void setRated(String rated) {
-        Rated = rated;
-    }
-
-    public String getDirector() {
-        return Director;
-    }
-
-    public void setDirector(String director) {
-        Director = director;
-    }
-
-    public String getAwards() {
-        return Awards;
-    }
-
-    public void setAwards(String awards) {
-        Awards = awards;
-    }
-
-    public String getPlot() {
-        return Plot;
-    }
-
-    public void setPlot(String plot) {
-        Plot = plot;
-    }
-
-    public String getBoxOffice() {
-        return BoxOffice;
-    }
-
-    public void setBoxOffice(String boxOffice) {
-        BoxOffice = boxOffice;
-    }
-
-    public String getActors() {
-        return Actors;
-    }
-
-    public void setActors(String actors) {
-        Actors = actors;
-    }
-
-    public int getVotes() {
-        return votes;
-    }
-
-    public void setVotes(int votes) {
-        this.votes = votes;
-    }
 }
