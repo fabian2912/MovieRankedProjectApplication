a3eb252f9c5eb1a880b8d1f066c1064e2ded256a
diablolaw <94478511+diablolaw@users.noreply.github.com>
2023-02-02 16:56:09 +0000

    add @CrossOrigin

--- src/main/java/com/example/movieRankedProject/Controller/MovieSearchController.java
+++ src/main/java/com/example/movieRankedProject/Controller/MovieSearchController.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.websocket.server.PathParam;
@@ -19,6 +16,7 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/movie")
 public class MovieSearchController {
 
